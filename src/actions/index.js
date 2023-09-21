b0aebe71ad2202d85be51051064340d61b3d8ef9
I.Alfer <ivan.alfer@innowise-group.com>
2023-09-21 10:28:36 +0200

    init

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+export const heroesFetching = () => {
+    return {
+        type: 'HEROES_FETCHING'
+    }
+}
+
+export const heroesFetched = (heroes) => {
+    console.log("!!!" + heroes)
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+}
+
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
